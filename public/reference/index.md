39f216e3fd18bf71f53d7a951c0da85463db72f3
Lucas Souza <lucas.souza@ifood.com.br>
2021-08-30 20:59:27 -0300

    set Intro component

--- public/reference/index.md
+++ public/reference/index.md
@@ -1,6 +1,5 @@
 
-  Fylo stores all your most important files in one secure location. Access them wherever 
-  you need, share and collaborate with friends family, and co-workers.
+  
 
   Get Started
 
--- src/components/main/IntroSection.js
+++ src/components/main/IntroSection.js
@@ -5,6 +5,7 @@ const Section = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   background-color: hsl(218, 28%, 13%);
 `;
 
@@ -13,16 +14,52 @@ const IntroImage = styled.img`
 `;
 
 const IntroTitle = styled.h2`
-  max-width: 450px;
+  @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap");
+
+  font-family: "Raleway", sans-serif;
+  font-weight: 700;
+  font-size: 30px;
+  color: white;
+  text-align: center;
+  max-width: 380px;
+  padding: 30px;
+`;
+
+const IntroDescription = styled.p`
+  font-size: 19px;
+  color: white;
+  text-align: center;
+  padding: 0 36px 35px 36px;
+  max-width: 400px;
+`;
+
+const IntroButton = styled.button`
+  padding: 20px 120px;
+  border-radius: 50px;
+  border: none;
+  background: linear-gradient(to right, hsl(176, 68%, 64%), hsl(198, 60%, 50%));
+  color: white;
+  font-size: 18px;
+  font-weight: 700;
+  cursor: pointer;
 `;
 
 export default function Intro() {
   return (
     <Section>
       <IntroImage src={IntroIllustration} alt="Collaboration illustration" />
-      <h2> All your files in one secure location, accessible anywhere.</h2>
-      <p></p>
-      <button></button>
+
+      <IntroTitle>
+        All your files in one secure location, accessible anywhere.
+      </IntroTitle>
+
+      <IntroDescription>
+        Fylo stores all your most important files in one secure location. Access
+        them wherever you need, share and collaborate with friends, family and
+        co-workers.
+      </IntroDescription>
+
+      <IntroButton>Get Started</IntroButton>
     </Section>
   );
 }
